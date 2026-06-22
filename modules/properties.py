79d59a41ee918da9a162cc847afea48e46d60226
Ville Seppänen <140543300+villesepp@users.noreply.github.com>
2026-06-22 20:28:24 +0300

    Add files via upload

--- modules/properties.py
+++ modules/properties.py
@@ -1,11 +1,139 @@
-import bpy
+import bpy, os, functools
 from bpy.app.handlers import persistent
 from bpy.props import *
 from bpy.types import PropertyGroup
-
 from . import modifier_categories
+from pathlib import Path
 
+assets_dic = []
+BLENDER_VERSION_MAJOR_POINT_MINOR = float(bpy.app.version_string[0:4].strip("."))
 
+# should read all .blend files and get a hash based on file amounts, names, size, modifierd dated etc and only updated if hash does not match!
+
+def get_asset_blend_files():
+    prefs = bpy.context.preferences
+    filepaths = prefs.filepaths
+    asset_libraries = filepaths.asset_libraries
+    blend_file_paths = []
+
+    for asset_library in asset_libraries:
+        if BLENDER_VERSION_MAJOR_POINT_MINOR >= 5.2: 
+            if not asset_library.enabled:
+                continue
+        library_name = asset_library.name
+        library_path = Path(asset_library.path)
+        blend_files = [fp for fp in library_path.glob("**/*.blend") if fp.is_file()]
+
+        essentials = Path(os.path.join(bpy.app.binary_path.replace("blender.exe", ""), str(BLENDER_VERSION_MAJOR_POINT_MINOR), "datafiles", "assets", "nodes"))
+        essentials_blend_files = [fp for fp in essentials.glob("**/*.blend") if fp.is_file()]
+        blend_files.extend(essentials_blend_files)
+        blend_file_paths.extend(blend_files)
+    return blend_file_paths
+
+def get_assets_asset_library_hash(paths):
+    data = []
+
+    for path in paths:
+        data.append(os.path.getsize(path))
+        data.append(os.path.getctime(path))
+        data.append(os.path.getmtime(path))
+        data.append(os.path.basename(path))
+    data.append(len(paths))
+
+    return hash(str(data))
+    
+def has_asset_library_been_updated(paths): # we should do it per asset liberay!
+    hash_value = get_assets_asset_library_hash(paths)
+    return False    
+
+# def get_assset_libery_modifiers(blend_files):
+#     for filepath in blend_files:
+#         try:
+#             with bpy.data.libraries.load(str(filepath), assets_only=True) as (file_contents, _):
+#                 for ng_name in file_contents.node_groups:
+#                     asset_library_identifier = library_name
+#                     blend = str(os.path.basename(blend_file))
+#                     for item in file_contents.node_groups:
+#                         relative_asset_identifier = os.path.join(blend, "NodeTree", str(item))
+#                         asset_library_type = "CUSTOM"
+#                         if blend_file in essentials_blend_files:
+#                             asset_library_type = "ESSENTIALS"
+#                         assets_dic.append(
+#                         {
+#                             "name": os.path.basename(relative_asset_identifier).replace(".blend", ""),
+#                             "relative_asset_identifier": relative_asset_identifier,
+#                             "asset_library_identifier": asset_library_identifier,
+#                             "asset_library_type": asset_library_type,
+#                         })
+#         except Exception as e:
+#             print(f"Could not read asset file {filepath.name}: {e}")
+
+def get_all_modifier_assets():
+    global assets_dic
+
+    #  Scan the local current file, needs to be updated if new node gruope is added! 
+    for ng in bpy.data.node_groups:
+        assets_dic.append(
+        {
+            "name": ng.name,
+            "relative_asset_identifier": "NodeTree\\"+ng.name,
+            "asset_library_identifier": "",
+            "asset_library_type": 'LOCAL',
+        })
+
+    if not assets_dic:
+        prefs = bpy.context.preferences
+        filepaths = prefs.filepaths
+        asset_libraries = filepaths.asset_libraries
+
+        essentials = Path(os.path.join(bpy.app.binary_path.replace("blender.exe", ""), str(BLENDER_VERSION_MAJOR_POINT_MINOR), "datafiles", "assets", "nodes"))
+        essentials_blend_files = [fp for fp in essentials.glob("**/*.blend") if fp.is_file()]
+
+        for asset_library in asset_libraries: # user asset liberays
+            if BLENDER_VERSION_MAJOR_POINT_MINOR >= 5.2: 
+                if not asset_library.enabled:
+                    continue
+            library_name = asset_library.name
+            library_path = Path(asset_library.path)
+            blend_files = [fp for fp in library_path.glob("**/*.blend") if fp.is_file()]
+
+            if has_asset_library_been_updated(blend_files):
+                for blend_file in blend_files:
+                    with bpy.data.libraries.load(str(blend_file), assets_only=True) as (file_contents, _):
+                        asset_library_identifier = library_name
+                        blend = str(os.path.basename(blend_file))
+                        for item in file_contents.node_groups:
+                            relative_asset_identifier = os.path.join(blend, "NodeTree", str(item))
+                            asset_library_type = "CUSTOM"
+                            if blend_file in essentials_blend_files:
+                                asset_library_type = "ESSENTIALS"
+                            assets_dic.append(
+                            {
+                                "name": os.path.basename(relative_asset_identifier).replace(".blend", ""),
+                                "relative_asset_identifier": relative_asset_identifier,
+                                "asset_library_identifier": asset_library_identifier,
+                                "asset_library_type": asset_library_type,
+                            })
+        # essentials asset liberays
+    
+    return assets_dic
+# bpy.ops.object.modifier_add_node_group(asset_library_type='ESSENTIALS', asset_library_identifier="", relative_asset_identifier="nodes\\procedural_hair_node_assets.blend\\NodeTree\\Curl Hair Curves")
+
+def get_assets(prop):
+    assets = get_all_modifier_assets()
+    # prop.clear()
+    for item in assets:
+        try:
+            new_item = prop.add()
+            new_item.name = item["name"]
+            new_item.value = item["relative_asset_identifier"]
+            new_item.library = item["asset_library_identifier"]
+            new_item.asset_library_type = item["asset_library_type"]
+            new_item.is_asset = True
+        except:
+            pass
+            # path = item["relative_asset_identifier"]
+            # print(f"failed to add asset: {path}")
 # Callbacks
 # ======================================================================
 
@@ -64,6 +192,7 @@ def set_all_modifier_collection_items():
             item.name = name
             item.value = mod
 
+    # bpy.app.timers.register(functools.partial(get_assets, all_modifiers), first_interval=1.1)
 
 def set_mesh_modifier_collection_items():
     """This is to be called on loading a new file or reloading addons
@@ -78,6 +207,8 @@ def set_mesh_modifier_collection_items():
             item = mesh_modifiers.add()
             item.name = name
             item.value = mod
+
+    # bpy.app.timers.register(functools.partial(get_assets, mesh_modifiers), first_interval=1.2)
 
 
 def set_curve_text_modifier_collection_items():
@@ -94,6 +225,7 @@ def set_curve_text_modifier_collection_items():
             item.name = name
             item.value = mod
 
+    # bpy.app.timers.register(functools.partial(get_assets, curve_and_text_modifiers), first_interval=1.3)
 
 def set_curves_modifier_collection_items():
     """This is to be called on loading a new file or reloading addons
@@ -109,6 +241,7 @@ def set_curves_modifier_collection_items():
             item.name = name
             item.value = mod
 
+    # bpy.app.timers.register(functools.partial(get_assets, curves_modifiers), first_interval=1.4)
 
 def set_lattice_modifier_collection_items():
     """This is to be called on loading a new file or reloading addons
@@ -124,6 +257,7 @@ def set_lattice_modifier_collection_items():
             item.name = name
             item.value = mod
 
+    # bpy.app.timers.register(functools.partial(get_assets, lattice_modifiers), first_interval=1.5)
 
 def set_pointcloud_modifier_collection_items():
     """This is to be called on loading a new file or reloading addons
@@ -138,6 +272,8 @@ def set_pointcloud_modifier_collection_items():
             item = pointcloud_modifiers.add()
             item.name = name
             item.value = mod
+
+    # bpy.app.timers.register(functools.partial(get_assets, pointcloud_modifiers), first_interval=1.6)
 
 
 def set_surface_modifier_collection_items():
@@ -154,6 +290,8 @@ def set_surface_modifier_collection_items():
             item.name = name
             item.value = mod
 
+    # bpy.app.timers.register(functools.partial(get_assets, surface_modifiers), first_interval=1.7)
+
 
 def set_volume_modifier_collection_items():
     """This is to be called on loading a new file or reloading addons
@@ -168,6 +306,8 @@ def set_volume_modifier_collection_items():
             item = volume_modifiers.add()
             item.name = name
             item.value = mod
+
+    # bpy.app.timers.register(functools.partial(get_assets, volume_modifiers), first_interval=1.8)
 
 
 @persistent
@@ -189,13 +329,30 @@ def add_modifier(self, context):
 
     if mod_name == "":
         return None
+    
+    search_collections = (
+        ml_props.mesh_modifiers,
+        ml_props.curve_text_modifiers,
+        ml_props.curves_modifiers,
+        ml_props.lattice_modifiers,
+        ml_props.pointcloud_modifiers,
+        ml_props.surface_modifiers,
+        ml_props.volume_modifiers,
+        ml_props.all_modifiers,
+    )
+    modifier_item = next(
+        (item for collection in search_collections
+         if (item := collection.get(mod_name)) is not None),
+        None,
+    )
+    if modifier_item is None:
+        return None
 
-    mod_type = ml_props.all_modifiers[mod_name].value
-    bpy.ops.object.ml_modifier_add('INVOKE_DEFAULT', modifier_type=mod_type)
-
+    bpy.ops.object.ml_modifier_add('EXEC_DEFAULT', modifier_type=modifier_item.value)
     # Executing an operator via a function doesn't create an undo event,
     # so it needs to be added manually.
     bpy.ops.ed.undo_push(message="Add Modifier")
+    ml_props.modifier_to_add_from_search = ""
 
 
 # Modifier collections
@@ -204,41 +361,63 @@ def add_modifier(self, context):
 class AllModifiersCollection(PropertyGroup):
     # Collection Property for search
     value: StringProperty(name="Type")
-
+    # library: StringProperty(name="Library", default="")
+    # is_asset: BoolProperty(name="Is Asset?", default=False)
+    # asset_library_type: StringProperty(name="asset_library_type")
 
 class MeshModifiersCollection(PropertyGroup):
     # Collection Property for search
     value: StringProperty(name="Type")
-
+    # library: StringProperty(name="Library", default="")
+    # is_asset: BoolProperty(name="Is Asset?", default=False)
+    # asset_library_type: StringProperty(name="asset_library_type")
 
 class CurveTextModifiersCollection(PropertyGroup):
     # Collection Property for search
     value: StringProperty(name="Type")
+    # library: StringProperty(name="Library", default="")
+    # is_asset: BoolProperty(name="Is Asset?", default=False)
+    # asset_library_type: StringProperty(name="asset_library_type")
 
 
 class CurvesModifiersCollection(PropertyGroup):
     # Collection Property for search
     value: StringProperty(name="Type")
+    # library: StringProperty(name="Library", default="")
+    # is_asset: BoolProperty(name="Is Asset?", default=False)
+    # asset_library_type: StringProperty(name="asset_library_type")
 
 
 class LatticeModifiersCollection(PropertyGroup):
     # Collection Property for search
     value: StringProperty(name="Type")
+    # library: StringProperty(name="Library", default="")
+    # is_asset: BoolProperty(name="Is Asset?", default=False)
+    # asset_library_type: StringProperty(name="asset_library_type")
 
 
 class PointcloudModifiersCollection(PropertyGroup):
     # Collection Property for search
     value: StringProperty(name="Type")
+    # library: StringProperty(name="Library", default="")
+    # is_asset: BoolProperty(name="Is Asset?", default=False)
+    # asset_library_type: StringProperty(name="asset_library_type")
 
 
 class SurfaceModifiersCollection(PropertyGroup):
     # Collection Property for search
     value: StringProperty(name="Type")
+    # library: StringProperty(name="Library", default="")
+    # is_asset: BoolProperty(name="Is Asset?", default=False)
+    # asset_library_type: StringProperty(name="asset_library_type")
 
 
 class VolumeModifiersCollection(PropertyGroup):
     # Collection Property for search
     value: StringProperty(name="Type")
+    # library: StringProperty(name="Library", default="")
+    # is_asset: BoolProperty(name="Is Asset?", default=False)
+    # asset_library_type: StringProperty(name="asset_library_type")
 
 
 # Property groups
@@ -334,6 +513,8 @@ def register():
     set_pointcloud_modifier_collection_items()
     set_surface_modifier_collection_items()
     set_volume_modifier_collection_items()
+    # if not assets_dic:
+    #     bpy.app.timers.register(functools.partial(get_all_modifier_assets), first_interval=1.0)
 
 
 def unregister():
